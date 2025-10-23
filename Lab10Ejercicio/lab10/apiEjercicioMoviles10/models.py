55151b274dc8f8c1d80e569055754efb8ca80726
unknown <diego.medina.l@tecsup.edu.pe>
2025-10-23 14:05:25 -0500

    Django API

---
+++ Lab10Ejercicio/lab10/apiEjercicioMoviles10/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    thumbnail = models.URLField()
+
+    def __str__(self):
+        return self.title
